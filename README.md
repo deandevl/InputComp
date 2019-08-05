ada6ffdedbe95874f43f45a2b1cf93a282e2ff5e
Rick <deanr3@bardstown.com>
2019-08-05 07:05:29 -0400

    added demo folder; updated README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **input-comp** is a Vue.js (version >= 2.5) component similar to an html input element with alternate layouts for its labeling and a number of css variables for quick styling.  In addition, an event is provided to let the component's parent be notified of a change in value.
 
-**input-comp** depends on the [vue.js](https://vuejs.org/ "Vue.js") framework and can be installed via [npm install](https://docs.npmjs.com/cli/install.html "npm install") with the included `package.json` file.  Three [webpack](https://webpack.js.org/concepts/) npm scripts are included for building  development, production, or hot recompile/execute of the demo.  `build-dev` and `build-prod` scripts produce  a `dist` folder containing the `index.html`.  The size of the bundle using `build-prod` is 13 KiB with a CDN for incorporating the Vue framework.
+**input-comp** can be installed via with the included `package.json` file for a local installation via the [npm install](https://docs.npmjs.com/cli/install.html "npm install") command.  **input-comp** depends on the [vue.js](https://vuejs.org/ "Vue.js") framework.  A demo folder is provided that used [Parcel](https://parceljs.org/) together with its associated `package.json` file to bundle together  **input-comp's** [github location](https://github.com/deandevl/ActiveListComp.git) with its [vue.js](https://vuejs.org/ "Vue.js") dependency for a simple application.  Further details are provided below for running the demo.
 
 ## Props ##
 A prop in Vue.js is a custom attribute for passing information from a parent component hosting **input-comp** instance(s) to a **input-comp** as a child component. 
@@ -66,7 +66,7 @@ The parent component can listen to the above event and get the current input val
 ## Demonstration ##
 A demonstration of **input-comp** is provided in the [InputComp](https://github.com/deandevl/InputComp "InputComp") repository by hosting the `index.html`file under the `dist` folder.  The demo (templated in the `App.vue` file) hosts **input-comp**'s different layouts and types along with component styling and event handling by the parent.
 
-As a suggestion, install [http-server](https://www.npmjs.com/package/http-server "http-server") globally via [npm](https://www.npmjs.com/ "npm") then enter the command `http-server`in the **input-comp** `dist` directory.  From a browser enter the url: `localhost:8080/` to view the demo.
+As a suggestion, install [http-server](https://www.npmjs.com/package/http-server "http-server") locally/globally via [npm](https://www.npmjs.com/ "npm") then enter the command `http-server`in the **input-comp** `dist` directory.  From a browser enter the url: `localhost:8080/` to view the demo.
 
 Here is some example code for using **input-comp** taken from `App.vue`:
 
---
+++ demo/src/App.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="demo_container">
+    <button v-on:click="set_job">Set Job to 'Janitor'</button>
+    <button v-on:click="show_values">Show Current Values (at the console)</button>
+    <input-comp
+        heading="Enter your job"
+        header_position="below"
+        placeholder="Enter job here"
+        input_size="40"
+        :input_value="current_job"
+        :css_variables="css_variables"
+        v-on:input_comp_value_changed="value => {this.current_job = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Enter your name"
+        placeholder="Enter name here"
+        input_size="70"
+        header_position="below"
+        :input_value="current_name"
+        :single_border="single_border"
+        :css_variables="css_variables"
+        v-on:input_comp_value_changed="value => {this.current_name = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Enter your address"
+        placeholder="Enter address here"
+        input_size="90"
+        header_position="above"
+        :input_value="current_address"
+        :css_variables="css_variables_wide"
+        v-on:input_comp_value_changed="value => {this.current_address = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Enter your age:"
+        placeholder="Enter age here"
+        input_type="number"
+        input_size="40"
+        :input_value="current_age"
+        :css_variables="css_variables_wide"
+        v-on:input_comp_value_changed="value => {this.current_age = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Enter your birth date:"
+        input_type="date"
+        :input_value="current_birth_date"
+        :css_variables="css_variables_extra_wide"
+        :single_border="single_border"
+        v-on:input_comp_value_changed="value => {this.current_birth_date = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Do you smoke?"
+        input_type="checkbox"
+        :input_checked="current_smoker"
+        :css_variables="css_variables_wide"
+        v-on:input_comp_value_changed="value => {this.current_smoker = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Time you eat dinner?"
+        input_type="time"
+        :input_value="current_dinner_time"
+        :css_variables="css_variables_wide"
+        v-on:input_comp_value_changed="value => {this.current_dinner_time = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Month/Year of Collage Graduation:"
+        input_type="month"
+        :input_value="current_graduation"
+        :css_variables="css_variables_extra_wide"
+        v-on:input_comp_value_changed="value => {this.current_graduation = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Select your favorite color:"
+        input_type="color"
+        :input_value="current_color"
+        :css_variables="css_variables_wide"
+        v-on:input_comp_value_changed="value => {this.current_color = value}">
+    </input-comp>
+
+    <input-comp
+        heading="Your email address:"
+        input_type="email"
+        input_size="40"
+        :input_value="current_email"
+        :css_variables="css_variables_extra_wide"
+        v-on:input_comp_value_changed="value => {this.current_email = value}">
+    </input-comp>
+  </div>
+</template>
+
+<script>
+  import InputComp from 'inputcomp';
+
+  export default {
+    name: 'App',
+    data: function() {
+      return {
+        single_border: true,
+
+        current_job: 'Pipe Fitter',
+        current_name: null,
+        current_address: null,
+        current_age: 56,
+        current_birth_date: null,
+        current_smoker: true,
+        current_dinner_time: '17:30:00',
+        current_graduation: '1972-08',
+        current_color: '#6770d8',
+        current_email: null,
+
+        css_variables: {
+          input_comp_heading_color: 'white',
+          input_comp_input_color: 'white',
+          input_comp_input_border_color: 'white',
+          input_comp_input_placeholder_color: 'white',
+          input_comp_input_focus_background: 'black'
+        },
+        css_variables_wide: {
+          input_comp_heading_color: 'white',
+          input_comp_input_color: 'white',
+          input_comp_input_border_color: 'white',
+          input_comp_input_placeholder_color: 'white',
+          input_comp_input_focus_background: 'black',
+          input_comp_width: '20rem'
+        },
+        css_variables_extra_wide: {
+          input_comp_heading_color: 'white',
+          input_comp_input_color: 'white',
+          input_comp_input_border_color: 'white',
+          input_comp_input_placeholder_color: 'white',
+          input_comp_input_focus_background: 'black',
+          input_comp_width: '30rem'
+        }
+      };
+    },
+    components: {
+      InputComp
+    },
+    methods: {
+      show_values: function(){
+        console.log(`--------Current Values---------\n
+      ---Job: ${this.current_job}\n
+      ---Name: ${this.current_name}\n
+      ---Address: ${this.current_address}\n
+      ---Age: ${this.current_age}\n
+      ---Birth Date: ${this.current_birth_date}\n
+      ---Smoker?: ${this.current_smoker}\n
+      ---Eat Dinner Time: ${this.current_dinner_time}\n
+      ---Graduation Date: ${this.current_graduation}\n
+      ---Favorite Color: ${this.current_color}\n
+      ---Email address: ${this.current_email}\n
+      `);
+      },
+      // set the value of component InputComp for job to 'Janitor'
+      set_job: function(){
+        this.current_job = 'Janitor';
+      }
+    }
+  }
+</script>
+
+<style>
+  .demo_container {
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    color: white;
+    background-color: black;
+    padding: 20px;
+  }
+  button {
+    width: 260px;
+    height: 22px;
+  }
+</style>
